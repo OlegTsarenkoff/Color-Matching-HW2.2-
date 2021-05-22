e0007500ea5f3ed8f8cd5c38401f27f8c1f55e8c
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-05-22 21:29:21 +0300

    УРА! День был потрачен не зря! :)

--- Color Matching/ViewController.swift
+++ Color Matching/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var outputUsersColor: UIView!
+    
+    @IBOutlet weak var alphaCounter: UILabel!
+    @IBOutlet weak var redCounter: UILabel!
+    @IBOutlet weak var greenCounter: UILabel!
+    @IBOutlet weak var blueCounter: UILabel!
+    
+    @IBOutlet weak var alphaSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    private func outputColor() {
+        
+        let alphaColorCurrent = String(format: "%.2f", alphaSlider.value)
+        alphaCounter.text = alphaColorCurrent
+        let redColorCurrent = String(format: "%.2f", redSlider.value)
+        redCounter.text = redColorCurrent
+        let greenColorCurrent = String(format: "%.2f", greenSlider.value)
+        greenCounter.text = greenColorCurrent
+        let blueColorCurrent = String(format: "%.2f", blueSlider.value)
+        blueCounter.text = blueColorCurrent
+        
+        outputUsersColor.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        outputUsersColor.layer.cornerRadius = 20
+        outputColor()
+        
     }
-
-
+    
+    @IBAction func alphaSlide() {
+        outputColor()
+    }
+    @IBAction func redSlide() {
+        outputColor()
+    }
+    @IBAction func greenSlide() {
+        outputColor()
+    }
+    @IBAction func blueSlide() {
+        outputColor()
+    }
+    
+    
 }
 
